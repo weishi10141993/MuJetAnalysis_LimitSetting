32623e73c035a0e7a454433005fc8326b23970a9
Luca <luca.pernie@cern.ch>
2018-03-21 00:39:08 +0100

    2016 2nd commit

--- macros/sh/send0.6000_T10000_0.sh
+++ macros/sh/send0.6000_T10000_0.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /afs/cern.ch/work/l/lpernie/H2a4Mu/DisplacedMuonJetAnalysis_2015/LIMITS/CMSSW_7_4_7/src/limits_2a4mu 
+cd /afs/cern.ch/work/l/lpernie/H2a4Mu/DisplacedMuonJetAnalysis_2015/LIMITS/CMSSW_8_1_0/src/limits_2a4mu/ 
 eval `scramv1 runtime -sh`
-combine -n .H2A4Mu_mA_0.6000_GeV_T10000_0 -m 125 -M HybridNew --rule CLs --testStat LHC -H ProfileLikelihood -s -1 -T 10000 Datacards/datacard_H2A4Mu_mA_0.6000_GeV.txt > macros/sh/OutPut_0.6000_T10000_0.txt 
+combine -n .H2A4Mu_mA_0.6000_GeV_T10000_0 -m 125 -M HybridNew --rule CLs --testStat LHC -s -1 -T 10000 --fork 50 Datacards/datacard_H2A4Mu_mA_0.6000_GeV.txt > macros/sh/OutPut_0.6000_T10000_0.txt 
